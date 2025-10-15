9d8ddb0b6327acbfee38d3f06bc21b4b383ce117
vvidushibe25-dotcom <vvidushi_be25@thapar.edu>
2025-10-16 01:10:51 +0530

    Add files via upload

---
+++ 3) 2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b) {
+        if (a > c)
+            printf("%d is the greatest.\n", a);
+        else
+            printf("%d is the greatest.\n", c);
+    } else {
+        if (b > c)
+            printf("%d is the greatest.\n", b);
+        else
+            printf("%d is the greatest.\n", c);
+    }
+
+    return 0;
+}
+
